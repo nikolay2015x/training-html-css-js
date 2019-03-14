9537866cb8c8a40e97edd34e052b2a4cae0e105c
Nikolay Chiruk <43150090+nikolay2015x@users.noreply.github.com>
2019-03-14 11:35:18 +0300

    Add files via upload

---
+++ js/glory.js
@@ -0,0 +1,7 @@
+document.getElementById('glory').addEventListener('click' , function(){
+    document.querySelector('.portfolio-none').style.display = 'block';
+});
+
+document.getElementById('video-button').addEventListener('click' , function(){
+    document.querySelector('.video-none').style.display = 'block';
+});
