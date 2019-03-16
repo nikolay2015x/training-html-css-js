ade13f7147179e377a80528fc854d73f7c06d4b1
nikolay2015x <nikolaiunited2015@mail.ru>
2019-03-16 15:51:15 +0300

    add: menu-mobile

--- js/glory.js
+++ js/glory.js
@@ -5,3 +5,12 @@ document.getElementById('glory').addEventListener('click' , function(){
 document.getElementById('video-button').addEventListener('click' , function(){
     document.querySelector('.video-none').style.display = 'block';
 });
+
+function openbox (box) {
+    display = document.getElementById('box').style.display;
+    if (display == 'block'){
+        document.getElementById('box').style.display = 'none';
+    } else {
+        document.getElementById('box').style.display = 'block';
+    }
+}
